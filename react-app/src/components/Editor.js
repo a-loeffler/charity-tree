bffecd232862459d2df419bbaea5a45c5bf2ec59
Drew Long <drewlong314@gmail.com>
2021-06-21 12:40:35 -0500

    basic Editor

---
+++ react-app/src/components/Editor.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import "../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+const EditorComponent = () => <Editor />;
+
+export default EditorComponent;
