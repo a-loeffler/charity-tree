d407093842ec74b3e09ab7eb46bea50aa45ba75f
Drew Long <drewlong314@gmail.com>
2021-06-21 17:23:35 -0500

    troublshooting

--- react-app/src/components/Editor.js
+++ react-app/src/components/Editor.js
@@ -1,14 +1,30 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useRef } from "react";
 import { Editor } from "react-draft-wysiwyg";
+import NavBar from "./NavBar";
 import "../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
-const EditorComponent = () => <Editor
-wrapperClassName="wrapper-class"
-// editorClassName="editor-class"
-// toolbarClassName="toolbar-class"
-wrapperStyle={<wrapperStyleObject>}
-{/* editorStyle={<editorStyleObject/>}
-toolbarStyle={<toolbarStyleObject/>} */}
-/>
 
+const DropDownComponent = () => {
+    return (
+        <form>
+            <input></input>
+        </form>
+    )
+}
+
+const EditorComponent = () => {
+    return <Editor />
+//   return (
+//     <Editor
+//       //   ref={editorReference}
+//       toolbar={{inline: { inDropdown: true },
+//       list: { inDropdown: true },
+//       textAlign: { inDropdown: true },
+//       link: { inDropdown: true },
+//       history: { inDropdown: true },
+//       image: {component: DropDownComponent}}}
+//       editorStyle={{ border: "1px solid", backgroundColor: "beige" }}
+//     ></Editor>
+//   );
+};
 export default EditorComponent;
