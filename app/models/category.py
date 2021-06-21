025595413f9d9d9ed80dac592d354ed2472b3b08
msineath <msineath@LAPTOP-65OGSQM1.localdomain>
2021-06-21 13:45:36 -0400

    added all models. next step: associations

--- app/models/category.py
+++ app/models/category.py
@@ -3,5 +3,6 @@ from .db import db
 
 class Category(db.Model):
     __tablename__ = 'categories'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
--- app/models/project.py
+++ app/models/project.py
@@ -3,6 +3,7 @@ from .db import db
 
 class Project(db.Model):
     __tablename__ = 'projects'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     description = db.Column(db.String(5000), nullable=False)
