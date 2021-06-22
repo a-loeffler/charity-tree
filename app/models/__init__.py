4738ed7f660159b9ca75e77e13cc5a9c5518ae2f
Drew Long <drewlong314@gmail.com>
2021-06-22 15:54:48 -0500

    new text editor

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,7 @@
 from .db import db
 from .user import User
-from .category import Category
-from .project import Project
-from .project_media import Project_media
-from .tiers import Tier
-from .donors import Donor
+# from .category import Category
+# from .project import Project
+# from .project_media import Project_media
+# from .tiers import Tier
+# from .donors import Donor
--- react-app/src/components/Editor.js
+++ react-app/src/components/Editor.js
@@ -1,24 +1,44 @@
-import React, { Component, useEffect, useRef } from "react";
-import { Editor } from "react-draft-wysiwyg";
+import React, { Component, useEffect, useRef, useState } from "react";
+import { Editor } from '@tinymce/tinymce-react';
+import ReactDOM from 'react-dom'
 import NavBar from "./NavBar";
 import "../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 
 const EditorComponent = () => {
-    return (
-        <Editor/>
-    )
-//   return (
-//     <Editor
-//       //   ref={editorReference}
-//       toolbar={{inline: { inDropdown: true },
-//       list: { inDropdown: true },
-//       textAlign: { inDropdown: true },
-//       link: { inDropdown: true },
-//       history: { inDropdown: true },
-//       image: {component: DropDownComponent}}}
-//       editorStyle={{ border: "1px solid", backgroundColor: "beige" }}
-//     ></Editor>
-//   );
+    const editorRef = useRef(null);
+   const log = () => {
+     if (editorRef.current) {
+       console.log(editorRef.current.getContent());
+     }
+   };
+   return (
+     <>
+       <Editor
+         onInit={(evt, editor) => editorRef.current = editor}
+         initialValue="<p>This is the initial content of the editor.</p>"
+         init={{
+           height: 500,
+           plugins: [
+             'advlist autolink lists link image charmap print preview anchor',
+             'searchreplace visualblocks code fullscreen',
+             'insertdatetime media table paste code help wordcount',
+            'image media'
+           ],
+           menubar:false,
+           toolbar: 'undo redo | formatselect | ' +
+           'bold italic backcolor | alignleft aligncenter ' +
+           'alignright alignjustify | bullist numlist outdent indent | ' +
+           'removeformat | help | image',
+           content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+           image_list: [
+            {title: 'My image 1', value: 'https://www.example.com/my1.gif'},
+            {title: 'My image 2', value: 'http://www.moxiecode.com/my2.gif'}
+        ],
+         }}
+       />
+       <button onClick={log}>Log editor content</button>
+     </>
+   );
 };
 export default EditorComponent;
