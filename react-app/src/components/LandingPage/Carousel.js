937eb5e202b505816cea76c25746c82a6972438a
Andrew Loeffler <aloeffler@gmail.com>
2021-06-22 19:42:54 -0400

    start work on carousels; basic structure complete

--- react-app/src/components/LandingPage/Carousel.js
+++ react-app/src/components/LandingPage/Carousel.js
@@ -3,8 +3,9 @@ import './index.css'
 
 import ProjectCard from './ProjectCard'
 
-const Carousel = () => {
+const Carousel = ({list}) => {
 
+    console.log(list)
 
 
     return (
@@ -14,19 +15,15 @@ const Carousel = () => {
                 <a className="carousel-title-link" href="">Link</a>
             </div>
             <div className="carousel-buttons-container">
-                <p>Buttons!</p>
+                <div className="carousel-button-border right-space">
+                    <img className="carousel-button" src="images/left-button.svg"></img>
+                </div>
+                <div className="carousel-button-border">
+                    <img className="carousel-button" src="images/right-button.svg"></img>
+                </div>
             </div>
             <div className="carousel-items-container">
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
+                {list.map(project => <ProjectCard title={project}/>)}
             </div>
         </div>
     )
