913fbae7b6998ccf0b5cafe1475d83a7d7c57687
kbetker <kevin@betker.org>
2021-06-23 18:32:12 -0500

    commiting signup form now for real

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -6,13 +6,23 @@ from app.models import User
 
 def user_exists(form, field):
     print("Checking if user exits", field.data)
-    email = field.data
-    user = User.query.filter(User.email == email).first()
+    username = field.data
+    print(field)
+    user = User.query.filter(User.username == username).first()
     if user:
         raise ValidationError("User is already registered.")
 
 
+def email_exists(form, field):
+    print("Checking if user exits", field.data)
+    email = field.data
+    print(field)
+    user = User.query.filter(User.email == email).first()
+    if user:
+        raise ValidationError("Email is already registered.")
+
+
 class SignUpForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired(), user_exists])
+    username = StringField('username', validators=[DataRequired(), user_exists])  # noqa
+    email = StringField('email', validators=[DataRequired(), email_exists])
     password = StringField('password', validators=[DataRequired()])
