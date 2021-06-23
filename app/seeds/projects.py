7e9bfe5268107605aba3d3564f905f6559a9348f
msineath <msineath@LAPTOP-65OGSQM1.localdomain>
2021-06-23 12:45:53 -0400

    working on extra seed data.

--- app/seeds/projects.py
+++ app/seeds/projects.py
@@ -4,7 +4,16 @@ from app.models import db, Project
 # Adds a demo category, you can add other categories here if you want
 def seed_projects():
 
-    demo_project = Project(name='Demo', description='demoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo', goal=500, deadline="2021-12-25", owner_id=1)
+    demo_project_1 = Project(name='Demo', description='demoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo', goal=500, deadline="2021-12-25", owner_id=1)
+    demo_project_2 = Project(name='Demo', description='demoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo', goal=500, deadline="2021-12-25", owner_id=1)
+    demo_project_3 = Project(name='Demo', description='demoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo', goal=500, deadline="2021-12-25", owner_id=1)
+    demo_project_4 = Project(name='Demo', description='demoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo', goal=500, deadline="2021-12-25", owner_id=1)
+    demo_project_5 = Project(name='Demo', description='demoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo', goal=500, deadline="2021-12-25", owner_id=1)
+    demo_project_6 = Project(name='Demo', description='demoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo', goal=500, deadline="2021-12-25", owner_id=1)
+    demo_project_7 = Project(name='Demo', description='demoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo', goal=500, deadline="2021-12-25", owner_id=1)
+    demo_project_8 = Project(name='Demo', description='demoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo', goal=500, deadline="2021-12-25", owner_id=1)
+    demo_project_9 = Project(name='Demo', description='demoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo', goal=500, deadline="2021-12-25", owner_id=1)
+    demo_project_10 = Project(name='Demo', description='demoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo', goal=500, deadline="2021-12-25", owner_id=1)
 
     db.session.add(demo_project)
 
