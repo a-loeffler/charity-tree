e8a5c1f9da74c663bb9909da3d5c7f7ce6b64895
msineath <msineath@LAPTOP-65OGSQM1.localdomain>
2021-06-24 17:12:28 -0400

    fixed seeder and added store category functionality/backend routes

--- react-app/src/components/Navigation/Navigation.js
+++ react-app/src/components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { login } from "../../store/session";
 import { logout } from "../../store/session";
 import { getAllProjects } from "../../store/allProjects";
-
+import { getAllCategories } from "../../store/allCategories";
 
 
 
@@ -17,6 +17,7 @@ function Navigation() {
   const dispatch = useDispatch();
   const user = useSelector(state => state.session.user)
   const allProjects = useSelector((state) => state.allProjects.projects)
+  const allCategories = useSelector((state) => state.allCategories.categories)
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -48,11 +49,12 @@ function Navigation() {
 
   //========== Get All Projects ==============
   useEffect(() => {
-    console.log(allProjects)
     if (allProjects.length === 0) {
       dispatch(getAllProjects())
     }
-    console.log(allProjects)
+    if (allCategories.length === 0) {
+      dispatch(getAllCategories())
+    }
   }, [])
 
   //================  Submenu =============
