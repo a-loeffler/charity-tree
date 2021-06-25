08a78e7cbfb6d02171a91a7ab09990c26d13d008
msineath <msineath@LAPTOP-65OGSQM1.localdomain>
2021-06-24 20:10:59 -0400

    added media to individual project page

--- react-app/src/components/ProjectPage/ProjectPage.js
+++ react-app/src/components/ProjectPage/ProjectPage.js
@@ -1,28 +1,37 @@
 import React from "react"
-import {useSelector, useDispatch} from "react-redux"
-import {useParams} from 'react-router-dom'
+import { useSelector, useDispatch } from "react-redux"
+import { useParams } from 'react-router-dom'
 // import 'main.css'
 
 export default function ProjectPage() {
     const dispatch = useDispatch();
-    const {id} = useParams();
+    const { id } = useParams();
     const project = useSelector(state => state.allProjects.projects[id])
     const category = useSelector(state => state.allCategories.categories)
-    console.log(`*****************${JSON.stringify(category)}***************`)
+    const project_medias = useSelector(state => state.MediaList.project_medias)
+    const project_medias2 = project_medias.filter(obj => {
+        console.log(`------------++++++++++++++++ ${JSON.stringify(obj)}`)
+        return obj['project_id'] === Number(id)
+    });
+    console.log(`*****************${JSON.stringify(project_medias2)}***************`)
     // const a = category[project.category_id]
+    
 
     return (
         <div>
-            {/* picture container flex, grid? */}
-            <div> 
-            {/* for each media, give div */}
-            </div>
-            <h1>{project?.name}</h1>
+            {project_medias2.map(item => {
+                return (
+                <div>
+                    <img src={item.media_url}></img>
+                </div>
+                )
+            })}
+            <h1>{project?.name}</h1>    
             <h1>{category[project?.category_id]?.name}</h1>
             <h1>{project?.description}</h1>
             <h1>{`$${project?.goal.toLocaleString("en-US")}`}</h1>
             <h1>{`$${project?.current_amount.toLocaleString("en-US")}`}</h1>
-            {/* TODO: div for completion percentage bar */}
+            {/* TODO: div for completion percentage */}
             <h1>{project?.deadline}</h1>
             <h1>{project?.owner}</h1>
             <form action="https://www.paypal.com/donate" method="post" target="_top">
