70edccea14005375d09699cd79a30c7537343687
Andrew Loeffler <aloeffler@gmail.com>
2021-06-26 19:14:49 -0400

    finish adding tiers in project creation

--- react-app/src/components/RedirectModal/index.js
+++ react-app/src/components/RedirectModal/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { useHistory } from 'react-router';
+
 
 import './index.css'
 
 
 const RedirectModal = () => {
 
+    const history = useHistory();
+    const [timeDone, setTimeDone] = useState(false)
 
-    
+
+
+    setTimeout(() => {
+        setTimeDone(true);
+    }, 2001)
+
+
+
+    if (timeDone) {
+        history.push('/signup')
+    }
 
 
     return (
         <div className="redirect-modal-container">
-            <h1 className="redirect-modal-title">Users must be logged in to create a project.</h1>
+            <h1 className="redirect-modal-title">Users must be logged in to create a project</h1>
+            <button className="return-button"></button>
         </div>
     )
 
