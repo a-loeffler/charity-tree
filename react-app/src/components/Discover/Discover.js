56fc9cacc6f9a6aa1fca5225822f013e739961dd
kbetker <kevin@betker.org>
2021-06-26 19:19:42 -0500

    styled dicover and the cards

--- react-app/src/components/Discover/Discover.js
+++ react-app/src/components/Discover/Discover.js
@@ -19,14 +19,19 @@ export default function Discover() {
     //     return(
     //         null
     //     )
-    // } 
+    // }
     console.log('***********************************', allProjects[0]?.name)
+    const limitText = (str) => str.length > 70 ? `${str.substring(0, 70)}...` : str;
+    let width = "300px"
+    let minHeight = "300px"
+    let display = "flex"
+
 
     return (
         <div className="parentGrid">
             {allProjects?.map(project => {
                 return (
-                    <ProjectCard key={project.id} title={project?.name} description={project?.description} cardId={project?.id} image={projectMedia?.filter(item => item.project_id === project.id
+                    <ProjectCard key={project.id} width={width} minHeight={minHeight} display={display} title={project?.name} description={limitText(project?.description)} cardId={project?.id} image={projectMedia?.filter(item => item.project_id === project.id
                     )[0]}/>
                 )
             })}
