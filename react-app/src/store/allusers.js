6a3fb9032140494d2901343610b6ed9017526ede
msineath <msineath@LAPTOP-65OGSQM1.localdomain>
2021-06-28 10:08:51 -0400

    working on update user info. paypal bug fix

--- react-app/src/store/allusers.js
+++ react-app/src/store/allusers.js
@@ -1,8 +1,14 @@
 const SET_ALL_USERS = "session/SET_USERS"
+const UPDATE_USER = "session/UPDATE_USER"
 
 const setAllUsers = (users) => ({
     type: SET_ALL_USERS,
     payload: users
+})
+
+const updateUser = (user) => ({
+    type: UPDATE_USER,
+    payload: user
 })
 
 export const getAllUsers = () => async (dispatch) => {
@@ -11,12 +17,29 @@ export const getAllUsers = () => async (dispatch) => {
     dispatch(setAllUsers(data))
 }
 
+export const updateUserInfo = (user) => async (dispatch) => {
+    const response = await fetch(`api/users/update`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+    const data = await response.json()
+    dispatch(updateUser(data))
+}
+
 const initialState = {users: []}
 
 export default function allUsersReducer(state = initialState, action) {
     switch (action.type) {
         case SET_ALL_USERS:
             return state.users = action.payload
+        case UPDATE_USER:
+            const newState = {...state}
+            const target = newState.allUsers.users.filter(item => item.id === action.payload.id)
+            target = action.payload
+            return newState
         default:
             return state;
     }
