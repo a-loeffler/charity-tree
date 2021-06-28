19d1d84352e541e4f7ecaeb7138cc7b23291636f
Andrew Loeffler <aloeffler@gmail.com>
2021-06-28 10:02:48 -0400

    minor edit

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -62,9 +62,9 @@ const LandingPage = () => {
   const mockFeaturedProjectData = {
     title: "We be broke, y’all!",
     bannerUrl: "https://i.ibb.co/YWvkzkf/mock-banner.png",
-    creator: "Poor Boy",
+    creator: "Poor Creators",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "The creators of Charity Tree are working to develop web software that can aid and support charities and nonprofit organizations.  Join us in helping others!",
   };
 
   const mockRecommendedData = {
