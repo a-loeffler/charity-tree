1c045f355834b5949a06d7c2305cbfec86863afe
Andrew Loeffler <aloeffler@gmail.com>
2021-07-27 17:49:47 -0400

    start about page

--- react-app/src/components/About/index.js
+++ react-app/src/components/About/index.js
@@ -2,8 +2,37 @@ import React from "react"
 
 const About = () => {
     return (
-        <div>
-            <h1>This is the About Page</h1>
+        <div className="about-page-container">
+            <h1 className="about-page-title">Charity Tree</h1>
+            <section className="about-page-section">
+                <h2 className="about-page-heading">Mission and Purpose</h2>
+                <div className="about-info-container">
+                    <div className="about-info-image-container">
+                        <img src="/images/tree-vector.svg"></img>
+                    </div>
+                    <div className="about-info-text-container">
+                        <p className="about-info-text">
+                        Charity Tree is here to give you a way to support your
+                        favorite charities and nonprofit organizations. <br/>
+                        We want to help the world 
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section className="about-page-section">
+                <h2 className="about-page-heading">The Creators</h2>
+                <div className="about-creators-container">
+                <div className="creator-bubble">
+                </div>
+                <div className="creator-bubble">
+                </div>
+                <div className="creator-bubble">
+                </div>
+                <div className="creator-bubble">
+                </div>
+                </div>
+            </section>
+  
         </div>
     )
 }
