41558b75328df2a2cf5198379b2ffa703dea15a5
msineath <mikesineath@gmail.com>
2021-08-28 01:09:38 -0400

    removed unused imports and variables from landingpage component

--- react-app/src/components/LandingPage/Carousel.js
+++ react-app/src/components/LandingPage/Carousel.js
@@ -1,7 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom'
-
 import './index.css'
 
 import ProjectCard from './ProjectCard'
