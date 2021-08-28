dba2d7fdf69f2e0e77a638d35e9ee125065125f4
msineath <mikesineath@gmail.com>
2021-08-28 01:19:41 -0400

    removed unused dependency from discover component

--- react-app/src/components/Discover/Discover.js
+++ react-app/src/components/Discover/Discover.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ProjectCard from "../LandingPage/ProjectCard";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import "./index.css"
 
 export default function Discover() {
