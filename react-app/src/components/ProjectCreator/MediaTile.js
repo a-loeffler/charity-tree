d5f7789fb4e7834373d32dd6cde920c48f1f1716
msineath <mikesineath@gmail.com>
2021-08-28 01:02:18 -0400

    fixed "no" alt errors coming from mediatile.js

--- react-app/src/components/ProjectCreator/MediaTile.js
+++ react-app/src/components/ProjectCreator/MediaTile.js
@@ -7,7 +7,7 @@ const MediaTile = ({url, index}) => {
 
     return (
         <div className="media-tile-container">
-            <img className="media-tile-img" src={url}></img>
+            <img className="media-tile-img" src={url} alt='A media upload error has occurred. Please double check your file size/resolution and try again.'></img>
             <div className="media-delete-button" id={`media-delete-${index}`} >
                 <p>x</p>
             </div>
