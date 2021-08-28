301172cce1c344ee5c9256f07fa28d619e65fdd8
msineath <mikesineath@gmail.com>
2021-08-28 01:35:47 -0400

    removed unused variable declaration for session.js

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -47,7 +47,7 @@ export const login = (email, password) => async (dispatch) => {
 }
 
 export const logout = () => async (dispatch) => {
-    const response = await fetch("/api/auth/logout", {
+    await fetch("/api/auth/logout", {
         headers: {
             "Content-Type": "application/json",
         }
