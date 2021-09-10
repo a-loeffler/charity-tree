11b9e498b54636a5c9217b218620b84e4c0f7d9a
msineath <mikesineath@gmail.com>
2021-09-10 01:01:10 -0400

    cleaned up prints

--- app/api/likes.py
+++ app/api/likes.py
@@ -8,7 +8,6 @@ likes_routes = Blueprint("likes", __name__)
 @likes_routes.route("/<int:id>")
 def get_user_likes(id):
     likes = Like.query.filter(Like.user_id == id).all()
-    print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&7', likes)
     return {"likes": [like.to_dict() for like in likes]}
 
 
@@ -23,17 +22,14 @@ def add_new_like(id):
     db.session.add(newLike)
     db.session.commit()
 
-    print('44444444444444444444', newLike.to_dict())
     return newLike.to_dict()
 
 
 @likes_routes.route("/<int:id>/remove", methods=["DELETE"])
 def delete_like(id):
     data = request.get_json()
-    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^', data)
 
     like = Like.query.filter(Like.user_id == id, Like.project_id == data).first()
-    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^', like)
 
     db.session.delete(like)
     db.session.commit()
